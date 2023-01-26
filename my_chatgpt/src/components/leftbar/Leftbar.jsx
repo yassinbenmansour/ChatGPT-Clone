6dbee5e4cb417ee5af58135f8d7ad0b7c86d501e
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2023-01-26 11:15:33 +0100

    add Icons

--- my_chatgpt/src/components/leftbar/Leftbar.jsx
+++ my_chatgpt/src/components/leftbar/Leftbar.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import {
+  DiscordIcon,
+  ExternalLinkIcon,
+  LogOutIcon,
+  PlusIcon,
+  SunIcon,
+} from "../../constants";
 
 export default function Leftbar() {
   return (
-    <div>Leftbar</div>
+    <div><DiscordIcon /></div>
   )
 }
