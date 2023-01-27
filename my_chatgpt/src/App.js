014cad349794c38229a4446e60e1bb9f6c4afef0
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2023-01-27 12:21:52 +0100

    new chat

--- my_chatgpt/src/App.js
+++ my_chatgpt/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Leftsection from './Components/leftsection/Leftsection'
 
 export default function App() {
   return (
     <div>
+      
+      <Leftsection />
 
       
       
--- my_chatgpt/src/Components/leftsection/Leftsection.jsx
+++ my_chatgpt/src/Components/leftsection/Leftsection.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import {
+    DiscordIcon,
+    ExternalLinkIcon,
+    LogOutIcon,
+    PlusIcon,
+    SunIcon,
+  } from "../../constants";
 
 export default function Leftsection() {
   return (
-    <div>Leftsection</div>
+    <div className='flex w-72 bg-black h-screen'>
+      <div className='m-2 w-full'>
+        <a href='new' className="flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm flex-shrink-0 border border-white/20">
+              <PlusIcon />
+              New chat
+        </a>
+      </div>
+    </div>
   )
 }
